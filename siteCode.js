538e1a6773f2a7423779e628e4afe6705cff919b
nathansolomon7 <85477782+nathansolomon7@users.noreply.github.com>
2021-06-09 12:37:53 -0700

    Update siteCode.js

--- siteCode.js
+++ siteCode.js
@@ -1,25 +1,37 @@
-//TODO: FIX UP CSS STYLE HEAVILY
-//GET SEARCH BAR WORKING FOR ALL PAGES BY CHANGING FROM ID TO class
+//MUST DO BY TOMORROW THE 9TH
 //ADD PAIGINATION
 // FIGURE OUT HOW TO GET MORE THAN 10 RESULTS
-
-
+// MUST HANDLE NOT HAVING AN IMAGE OR EMPTY FIELD - test with Inception for image handling
 // ADD TIME FILTER 
+// COMMENT OUT CODE
 
+//THINGS YOU CAN WAIT ON FOR OFFICE HOURS:
+//FIGURING OUT HOW TO DELETE THE MOST RECENT INPUT IN SEARCH BAR TO NOT HAVE INACTIVE SEARCH BAR 
+// FIX CSS FLEX BOX AND ADDRRESS ANY STYLING BUGS
  
  var searchString_global;
  var searchStringtemp;
  console.log("running:");
 //////////////////////////////////////////////////////////////////////////////////////////
+
+var searchBartextValues = document.getElementsByClassName('searchBartext');
+
+for(var x = 0; x < searchBartextValues.length; x++) {
+    searchBartextValues[x].addEventListener('keyup', (e) => {
+        searchStringtemp = (e.target.value);
+        searchString_global = searchStringtemp;
+        console.log(searchStringtemp);
+    });
+}
+
 function loadMovies(){
     try{
         console.log("running loadMovies()");
-        
-        var buttonReference_searchBartext = document.getElementsByClassName('searchBartext');
-            for(var x = 0; x < buttonReference_searchBartext.length; x++){
-                 searchStringtemp = buttonReference_searchBartext[x].value;
-            }
-        searchString_global = searchStringtemp;
+        // loadNextsearchHelper(searchString_global);
+        // var buttonReference_searchBartext = document.getElementsByClassName('searchBartext');
+        //     for(var x = 0; x < buttonReference_searchBartext.length; x++){
+        //          searchStringtemp = buttonReference_searchBartext[x].value;
+        //     }
         console.log(searchString_global);
         console.log("above is searchString_global in the loadMovies function");
         $.getJSON('https://www.omdbapi.com/?s=' + searchString_global + '&apikey=e5794361').then(function(response){
@@ -31,6 +43,12 @@ function loadMovies(){
      catch(err) {
         console.error(err);
     }
+};
+
+function grandLoadmoviesNextsearch(){
+    let nextMovieSearch = sessionStorage.getItem('nextMovieSearch');
+    searchString_global = nextMovieSearch;
+    loadMovies();
 };
 
 
@@ -56,6 +74,12 @@ function loadDetails(movieTitle){
         return false;
     
 };
+
+function loadNextsearchHelper(nextMovieSearch){
+    sessionStorage.setItem('nextMovieSearch',nextMovieSearch);
+    window.location = 'movieSite.html';
+    return false;
+}
 
 //////////////////////////////////////////////////////////////////////
 function displayMoviedetails() {
